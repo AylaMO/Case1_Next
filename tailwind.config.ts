00fd4b9dabb0865547850ba100013497c3936fed
Ayla10-kodehode <ayla10.kodehode@gmail.com>
2024-04-18 02:21:24 +0200

    tailconfig

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,11 +8,44 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        roboto: ['var(--font-roboto)']
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))"
       },
+
+      
+
+      screens: {
+        ms: "320px",
+
+        mm: "375px",
+
+        ml: "425px",
+
+        sm: "640px",
+
+        md: "768px",
+
+        lg: "1024px",
+
+        xl: "1280px",
+
+        "2xl": "1536px",
+      },
+
+      colors: {
+        white: "#EDF2F4",
+        grey: "#8C909B",
+        black: "#2B2D42",
+        blue: "#B4CDED",
+        greyGreen: "#BACDC1",
+        green: "#BFCC94",
+      },
+
     },
   },
   plugins: [],
